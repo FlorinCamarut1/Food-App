dddf3adc40c155e74a42984068340be71bfcfcf6
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-05-04 19:17:11 +0300

    i created a food app, i added visual sections as navbar, a cart a modal for cart and a list of products with add button

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import React, { useState, useEffect } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
-  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (storedUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
-
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <Fragment>
+      <Cart />
+      <Header />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        <Meals />
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
